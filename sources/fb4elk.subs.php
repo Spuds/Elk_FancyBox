4f71c0c4c39cfc180e149293f403f0deb8a81edf
spuds <spuds@spudsdesign.com>
2014-06-17 08:37:07 -0400

    ! update class names for 1.1 autoloader

--- sources/fb4elk.subs.php
+++ sources/fb4elk.subs.php
@@ -251,7 +251,7 @@ function fb4elk_settings()
 	$context[$context['admin_menu_name']]['tab_data']['tabs']['fancybox']['description'] = $txt['fancybox_desc'];
 
 	// Lets build a settings form
-	require_once(SUBSDIR . '/Settings.class.php');
+	require_once(SUBSDIR . '/SettingsForm.class.php');
 
 	// Instantiate the form
 	$fbSettings = new Settings_Form();
