5572cff759385cf6365c05ee159ef78970f1967e
spuds <spuds@spudsdesign.com>
2014-11-07 13:39:43 -0600

    ! add new display options ! add ability to lightbox images from some photo hosts ! fix issue where elk js bbc clicker was still active with fb

--- languages/fb4elk.english.php
+++ languages/fb4elk.english.php
@@ -2,7 +2,7 @@
 
 // Settings
 $txt['fancybox_enabled'] = 'Enable the Fancybox Addon';
-$txt['fancybox_enabled_desc'] = 'Fancybox JS is licensed under a Creative Commons Attribution-Non Commercial 3.0 License. This means you must get the author\'s permission and a commercial license to use Fancybox JS on a commercial website.<br /><br />Enabling fancybox means that you are agreeing to the <a href=http://fancyapps.com/fancybox/#license>Fancybox License</a> terms';
+$txt['fancybox_enabled_desc'] = '<strong>Fancybox JS</strong> is licensed under a Creative Commons Attribution-Non Commercial 3.0 License. This means you must get the author\'s permission and a commercial license to use Fancybox JS on a commercial website.<br /><br />Enabling fancybox means that you are agreeing to the <a href=http://fancyapps.com/fancybox/#license>Fancybox License</a> terms.<br /><br />To support postimage lookups, this addon makes use of <a href="http://www.ajax-cross-origin.com/"><strong>Ajax Cross Origin Plugin</strong></a> Licensed under the <a href="href=http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a>.';
 $txt['fancybox_title'] = 'FancyBox';
 $txt['fancybox_settings'] = 'FancyBox Settings';
 $txt['fancybox_desc'] = 'This addon adds a lightbox effect to attachment and bbc images.';
@@ -25,6 +25,7 @@ $txt['fancybox_navSpeed'] = 'Animation speed when moving to next image';
 $txt['fancybox_panel_position'] = 'Position navigation buttons';
 $txt['fancybox_panel_top'] = 'Top';
 $txt['fancybox_panel_bottom'] = 'Bottom';
+$txt['fancybox_panel_simple'] = 'Inside';
 $txt['fancybox_thumbnails'] = 'Display thumbnails below slides';
 
 // Other
@@ -32,6 +33,13 @@ $txt['fancybox_other'] = 'Other Options';
 $txt['fancybox_autoPlay'] = 'Slideshow will start after opening the first gallery item';
 $txt['fancybox_playSpeed'] = 'Slideshow speed in milliseconds ';
 $txt['fancybox_bbc_img'] = 'Apply fancybox to images added with the [img] bbc tag';
+$txt['fancybox_disable_img_in_url'] = 'Don\'t attach fancybox to linked BBC images';
+$txt['fancybox_convert_photo_share'] = 'Convert photo share links to display the full sized image';
+$txt['fancybox_convert_photo_share_help'] = 'Limited support for imageshack, photobucket, radikal, fotosik, postimage, flic.kr, smugmug';
+
+$txt['fancybox_displayOptions'] = 'Display Options';
+$txt['fancybox_Padding'] = 'Size of border to place around the image, 0 for none';
+$txt['fancybox_Loop'] = 'Enables cyclic navigation, so when you reach the last element, first element will be displayed';
 
 // Template strings for the gallery buttons
 $txt['fancy_text_error'] = 'Unable to load the requested image.';
--- sources/fb4elk.subs.php
+++ sources/fb4elk.subs.php
@@ -23,7 +23,7 @@ if (!defined('ELK'))
  */
 function ilt_fb4elk()
 {
-	global $context, $txt, $modSettings;
+	global $context, $modSettings;
 
 	// If its off, just return
 	if (empty($modSettings['fancybox_enabled']))
@@ -38,16 +38,36 @@ function ilt_fb4elk()
 	loadCSSFile(array('fancybox/jquery.fancybox.css', 'fancybox/jquery.fancybox-buttons.css'), array('stale' => '?v=2.1.5'));
 	loadJavascriptFile(array('fancybox/jquery.fancybox.min.js', 'fancybox/helpers/jquery.fancybox-buttons.js'), array('stale' => '?v=2.1.5'));
 
+	// BBC image links enabled, then remove the elk bbc image clicker
+	if (!empty($modSettings['fancybox_bbc_img']))
+		addInlineJavascript('$(document).ready(function() {$("img").off("click.elk_bbc");});');
+
 	// Gallery thumbnails as well?
 	if (!empty($modSettings['fancybox_thumbnails']))
 	{
-		// Lets remove the elk bbc image clicker
-		addInlineJavascript('$(document).ready(function() {$("img").off("click.elk_bbc");});');
-
-		// Load up FB
+		// Load up FB helpers
 		loadCSSFile('fancybox/jquery.fancybox-thumbs.css', array('stale' => '?v=2.1.5'));
 		loadJavascriptFile('fancybox/helpers/jquery.fancybox-thumbs.js', array('stale' => '?v=2.1.5'));
 	}
+
+	// And output the neeeded JS commands
+	build_javascript();
+
+	// Build a lookup for postimage
+	if (!empty($modSettings['fancybox_convert_photo_share']) && !empty($modSettings['fancybox_bbc_img']))
+	{
+		// CORS lib
+		loadJavascriptFile(array('fancybox/jquery.ajax-cross-origin.min.js'), array('stale' => '?v=2.1.5'));
+		build_lookup();
+	}
+}
+
+/**
+ * Builds the javascript ready function to enable fancybox
+ */
+function build_javascript()
+{
+	global $modSettings, $txt;
 
 	// Build the Javascript based on ACP choices
 	$javascript = '
@@ -58,11 +78,10 @@ function ilt_fb4elk()
 
 				tag.addClass("fancybox").removeAttr("onclick");
 
-				// no rel tag yet? then add one
+				// No rel tag yet? then add one
 				if (!tag.attr("rel"))
 					tag.attr("rel", "gallery")
 			});
-
 
 			// Find all the attachment / bbc divs on the page
 			$("div[id$=_footer]").each(function() {
@@ -74,9 +93,10 @@ function ilt_fb4elk()
 			// Attach FB to everything we tagged with the fancybox class
 			$(".fancybox").fancybox({
 				type: "image",
-				padding: 10,
+				padding: ' . $modSettings['fancybox_Padding'] . ',
 				arrows: true,
-				loop: false,
+				closeBtn: true,
+				loop: "' .  !empty($modSettings['fancybox_Loop']) . '",
 				openEffect: "' . $modSettings['fancybox_openEffect'] . '",
 				openSpeed: ' . (int) $modSettings['fancybox_openSpeed'] . ',
 				closeEffect: "' . $modSettings['fancybox_closeEffect'] . '",
@@ -86,13 +106,13 @@ function ilt_fb4elk()
 				prevEffect: "' . $modSettings['fancybox_navEffect'] . '",
 				prevSpeed: ' . (int) $modSettings['fancybox_navSpeed'] . ',
 				autoPlay: ' . (!empty($modSettings['fancybox_autoPlay']) ? 'true' : 'false') . ',
-				playSpeed: ' . (int) $modSettings['fancybox_playSpeed'] . ',
+				playSpeed: ' . (int) $modSettings['fancybox_playSpeed'] . ($modSettings['fancybox_panel_position'] !== 'simple' ? ',
 				tpl: {
 					error: \'<p class="errorbox">' . $txt['fancy_text_error'] . '</p>\',
 					closeBtn: \'<div title="' . $txt['find_close'] . '" class="fancybox-item fancybox-close"></div>\',
 					next: \'<a title="' . $txt['fancy_button_next'] . '" class="fancybox-item fancybox-next"><span></span></a>\',
 					prev: \'<a title="' . $txt['fancy_button_prev'] . '" class="fancybox-item fancybox-prev"><span></span></a>\'
-				},
+				},' : ',') . '
 				helpers: {
 					buttons: {
 						tpl: \'\'+
@@ -131,9 +151,49 @@ function ilt_fb4elk()
 					},';
 
 	$javascript .= '
+					ajax: {
+						dataType : \'html\',
+						headers  : { \'X-fancyBox\': true, \'User-Agent\': "' . $_SERVER['HTTP_USER_AGENT'] . '"}
+					},
 				}
 			});
 		});';
+
+	addInlineJavascript($javascript, true);
+}
+
+/**
+ * The only way I see to reliabley get the full size image link
+ * from postimage is to request it.  This makes the ajax request
+ * to find all postim links on a page and update them.
+ */
+function build_lookup()
+{
+	$javascript = '
+	$(\'a[href*="postim"]\').each(function (i, item) {
+		var $item = $(item),
+			href = $item.attr("href"),
+			regex = new RegExp("<img src=\'(.*?\\.(png|gif|jp(e)?g|bmp))\'", "gi");
+		$.ajax({
+			type: "GET",
+			dataType: "html",
+			crossOrigin: true,
+			url: href,
+			cache: true,
+			headers: {"User-Agent": "' . $_SERVER['HTTP_USER_AGENT'] . '"}
+		}).done(function (response) {
+			// There is an img in the response
+			if (response.indexOf(\'<img\') !== -1) {
+				match = regex.exec(response);
+				if (match !== null) {
+					// Swap out the old link with the correct one
+					$item.attr("href", match[1]);
+				}
+			}
+		}).fail(function (xhr, status) {
+			// Nothing much to do
+		});
+	})';
 
 	addInlineJavascript($javascript, true);
 }
@@ -175,7 +235,7 @@ function ibc_fb4elk(&$codes)
 
 			if ($code['content'] == '<img src="$1" alt="" class="bbc_img" />')
 				$code['content'] = '<a href="$1" class="fancybox" rel="topic"><img src="$1" alt="" class="bbc_img" /></a>';
-			elseif ($code['content'] == '<img src="$1" alt="{alt}" style="width:{width};height:{height}" class="bbc_img resized" />')
+			elseif ($code['content'] == '<img src="$1" alt="{alt}" style="{width}{height}" class="bbc_img resized" />')
 				$code['content'] = '<a href="$1" class="fancybox" title="{alt}" rel="topic"><img src="$1" alt="{alt}" style="' . $style . '" class="bbc_img resized fancybox" /></a>';
 		}
 	}
@@ -194,15 +254,75 @@ function ipdc_fb4elk(&$output, &$message)
 {
 	global $modSettings;
 
+	$regex = '~<a href="(.*)".*(class="bbc_link").*>(<a href="(.*)".*(class="fancybox" rel="topic")>)<img.*class="bbc_img" />(</a>(</a>))~';
+
 	// Make sure we need to do anything
 	if (empty($modSettings['enableBBC']) || empty($modSettings['fancybox_bbc_img']))
 		return;
+
+	// Fix nested links caused by [url=remote][img]http://remote[/img][/url]
+	// These occur as part of parse_bbc so deal with it
+	$output['body'] = preg_replace_callback($regex, 'fix_url_bbc', $output['body']);
 
 	// Find all the bbc images with a rel="topic" in the links and inject the gallery tag so
 	// the bbc images and attachments of a message are part of the same gallery
 	$output['body'] = str_replace('rel="topic"', 'rel="gallery_msg_' . $output['id'] . '_footer"', $output['body']);
 }
 
+/**
+ * Updates links to external sites to link to full image or reverts the nested link to
+ * be what it was since we add a link via the updated img BBC tag.
+ *
+ * @param mixed[] $matches from the regex with the following capture groups
+ *	[0] full match
+ *	[1] outside link href
+ *	[2] outside link class=""
+ *	[3] inside link full
+ *	[4] inside link href
+ *	[5] inside link class="fancybox" rel="topic"
+ *	[6] trailing </a></a>
+ *	[7] trailing </a>
+ */
+function fix_url_bbc($matches)
+{
+	global $modSettings;
+	static $linker;
+
+	$output = $matches[0];
+
+	// Don't want fancybox at all on linked bbc image [url=remote][img]http://remote[/img][/url] syntax
+	if (!empty($modSettings['fancybox_disable_img_in_url']))
+	{
+		// Remove the inside link and trailing </a>
+		$output = str_replace($matches[3], '', $output);
+		$output = str_replace($matches[6], $matches[6], $output);
+	}
+	// Fix the links so they link to what they did (ie the url)
+	else
+	{
+		// Remove the inside link
+		$output = str_replace($matches[3], '', $output);
+
+		// Swap outside link class with the inside one (fancybox)
+		$output = str_replace($matches[2], $matches[5], $output);
+
+		// Replace the double </a></a> with a single
+		$output = str_replace($matches[6], $matches[7], $output);
+
+		// Support remote image sites?
+		if (!empty($modSettings['fancybox_convert_photo_share']))
+		{
+			if (empty($linker))
+				$linker = new getRemoteLink();
+
+			$newlink = $linker->process_URL($matches);
+			if ($newlink)
+				$output = str_replace($matches[1], $newlink, $output);
+		}
+	}
+
+	return $output;
+}
 
 /**
  * iaa_fb4elk()
@@ -264,8 +384,30 @@ function fb4elk_settings()
 				fbThumb_dd = $(\'#fancybox_panel_position\'),
 				fbThumb_dt = $(\'#setting_fancybox_panel_position\');
 
+			var fbConvert = document.getElementById(\'fancybox_disable_img_in_url\'),
+				fbShare = document.getElementById(\'fancybox_convert_photo_share\'),
+				fbBBC = document.getElementById(\'fancybox_bbc_img\');
+
+			// Show the BBC url box only if bbc image option is enabled
+			if (fbBBC.checked === false)
+			{
+				fbConvert.disabled = true;
+				fbShare.disabled = true;
+			}
+			else
+			{
+				fbConvert.disabled = false;
+				fbShare.disabled = false;
+			}
+
+			// Show the convert image host sites if we are converting url-ed bbc image
+			if (fbConvert.checked === false && fbBBC.checked === true)
+				fbShare.disabled = false;
+			else
+				fbShare.disabled = true;
+
 			// Show the thumbnail position box only if the option has been selected
-			if (fbThumb === false)
+			if (fbThumb === true)
 			{
 				// dd and the dt
 				fbThumb_dd.parent().slideUp();
@@ -276,6 +418,7 @@ function fb4elk_settings()
 				fbThumb_dd.parent().slideDown();
 				fbThumb_dt.parent().slideDown();
 			}
+
 		}
 		showhidefbOptions();', true);
 
@@ -302,16 +445,21 @@ function fb4elk_settings()
 				'none' => $txt['fancybox_effect_none'])
 			),
 			array('int', 'fancybox_navSpeed'),
-		'',
+		array('title', 'fancybox_displayOptions'),
+			array('int', 'fancybox_Padding'),
+			array('check', 'fancybox_Loop'),
 			array('check', 'fancybox_thumbnails', 'onchange' => 'showhidefbOptions();'),
 			array('select', 'fancybox_panel_position', array(
 				'top' => $txt['fancybox_panel_top'],
-				'bottom' => $txt['fancybox_panel_bottom'])
+				'bottom' => $txt['fancybox_panel_bottom'],
+				'simple' => $txt['fancybox_panel_simple'])
 			),
 		array('title', 'fancybox_other'),
 			array('check', 'fancybox_autoPlay'),
 			array('int', 'fancybox_playSpeed'),
-			array('check', 'fancybox_bbc_img'),
+			array('check', 'fancybox_bbc_img', 'onchange' => 'showhidefbOptions();'),
+			array('check', 'fancybox_disable_img_in_url', 'onchange' => 'showhidefbOptions();'),
+			array('check', 'fancybox_convert_photo_share'),
 	);
 
 	// Load the settings to the form class
@@ -345,4 +493,178 @@ function fb4elk_settings()
 		updateSettings(array('fancybox_panel_position' => 'top'));
 
 	Settings_Form::prepare_db($config_vars);
+}
+
+/**
+ * Class to update external site links so the thumbnail expands to the full image
+ */
+class getRemoteLink
+{
+	protected $provider = array();
+	protected $providers = array();
+	protected $out = false;
+
+	/**
+	 * Determins if a link is from a host provider we support
+	 *
+	 * @return boolean
+	 */
+	private function _valid()
+	{
+		foreach ($this->providers as $host)
+		{
+			$host = trim($host);
+			if (stripos($this->provider, $host) !== false)
+			{
+				$this->provider = $host;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Loads the active image host providers
+	 *
+	 * @global string[] $modSettings
+	 */
+	private function _active_providers()
+	{
+		$this->providers = array('imageshack', 'photobucket', 'radikal', 'fotosik', 'postim', 'flickr', 'smugmug');
+	}
+
+	/**
+	 * Parses a URL in to a its parts so we have the host provider
+	 */
+	private function _parseURL()
+	{
+		$this->url_parts = parse_url($this->url[1]);
+
+		if ($this->url_parts !== false)
+			$this->provider = $this->url_parts['host'];
+		else
+			$this->provider = false;
+	}
+
+	/**
+	 * Main controlling function to update links
+	 *
+	 * @param string[] $url
+	 */
+	public function process_URL($url)
+	{
+		$this->url = $url;
+
+		// Sites we support
+		$this->_active_providers();
+
+		// Parse out a image host name for use
+		$this->_parseURL();
+
+		// A host we support and is active
+		if ($this->provider && $this->_valid())
+		{
+			// The the host function
+			$call = '_' . $this->provider;
+			$this->$call();
+		}
+
+		return $this->out;
+	}
+
+	/**
+	 * Imageshack changed how they works and I'm not sure this is the way to go for all links
+	 * but the old .th thing looks to be gone.
+	 *
+	 * thumbnail may look like http://imagizer.imageshack.us/v2/150x100q90/713/coreg.jpg
+	 * the link then is http://imageshack.us/download/713/coreg.jpg
+	 */
+	private function _imageshack()
+	{
+		$link_parts = parse_url($this->url[4]);
+
+		if ($link_parts !== false && isset($link_parts['path'], $link_parts['host']))
+		{
+			// break the path, we want the last two keys
+			$link = explode('/', $link_parts['path']);
+			$count = count($link);
+
+			// We have the last two, so lets build us a link
+			if ($count > 2)
+				$this->out = 'http://' . str_replace('imagizer.', '', $link_parts['host']) . '/download/' . $link[$count-2] . '/' . $link[$count-1];
+		}
+	}
+
+	/**
+	 * photobucket ... The link is to the album so just swap it, not sure there is a thumb one
+	 */
+	private function _photobucket()
+	{
+		$this->out = $this->url[4];
+	}
+
+	/**
+	 * The thumb link has a trailing t, remove that for a link to the full size
+	 */
+	private function _radikal()
+	{
+		if (preg_match('~(.*?)/([^/]*?)t\.(png|gif|jp(e)?g|bmp)$~isu', $this->url[4], $out))
+			$this->out = $out[1] . '/' . $out[2] . '.' . $out[3];
+	}
+
+	/**
+	 * [URL=http://www.fotosik.pl/pokaz_obrazek/cbf1f3a5b721a176.html]
+	 * [IMG]http://images70.fotosik.pl/318/cbf1f3a5b721a176m.jpg[/IMG]
+	 * [/URL]
+	 * Trailing m or med that gets trimmed for the full image link
+	 */
+	private function _fotosik()
+	{
+		if (preg_match('~(.*?)\.(?:m\.|)(png|gif|jp(e)?g|bmp)$~isu', $this->url[4], $out))
+		{
+			if (substr($out[1], -1) == 'm')
+				$out[1] = substr($out[1], 0, strlen($out[1]) - 1);
+			if (substr($out[1], -3) == 'med')
+				$out[1] = substr($out[1], 0, strlen($out[1]) - 3);
+			$this->out = $out[1] . '.' . $out[2];
+		}
+	}
+
+	/**
+	 * postimage
+	 * This service requries an external lookup to find the full size image
+	 * Currently done with JS lookups after page load, but this is here
+	 * should you want to do this server side.
+	 */
+	private function _postim()
+	{
+		require_once(SUBSDIR . '/Package.subs.php');
+
+		// So postimage cache is correct
+		//ini_set('user_agent', $_SERVER['HTTP_USER_AGENT']);
+		//$page = fetch_web_data($this->url[1]);
+
+		// Found the page, then find the link
+		//if ($page !== false && preg_match('~<img src=\'(.*?\.(png|gif|jp(e)?g|bmp))\'~is', $page, $link))
+		//	$this->out = $link[1];
+	}
+
+	/**
+	 * flic.kr can have a variety of smalls, here we look for _t _s or _q and swap it with _b
+	 */
+	private function _flic()
+	{
+		if (preg_match('~(.*?)(?:_[t|s|q]\.)(png|gif|jp(e)?g|bmp)$~isu', $this->url[4], $out))
+			$this->out = $out[1] . '_b.' . $out[2];
+	}
+
+	/**
+	 * Smugmug ... missing a recent example to test
+	 */
+	private function _smugmug()
+	{
+		if (preg_match('~(.*?)(?:\/S\/)(.*?)(?:-S)\.(png|gif|jp(e)?g|bmp)$~isu', $this->url[4], $out))
+			$this->out = $out[1] . '/O/' . $out[2] . '.' . $out[3];
+	}
 }
