db9b61b0bb59b8d6d4b68d2abad41abb1ea9c92c
spuds <spuds@spudsdesign.com>
2017-02-23 20:11:04 -0600

    ! add french translation

---
+++ languages/fb4elk.french.php
@@ -0,0 +1,52 @@
+<?php
+
+// Settings
+$txt['fancybox_enabled'] = 'Activer le module Fancybox';
+$txt['fancybox_enabled_desc'] = '<strong>Fancybox JS</strong> est distribué sous licence Creative Commons Attribution-Non Commercial 3.0. Vous devez donc demander la permission de l\'auteur et posséder une licence adaptée pour tout utilisation de Fancybox JS sur un site commercial.<br /><br />Activer fancybox signifie que vous acceptez sans réserve la <a href=http://fancyapps.com/fancybox/#license>Licence Fancybox</a>.<br /><br />Pour le support du site postimage, ce module utilise le plugin <a href="http://www.ajax-cross-origin.com/"><strong>Ajax Cross Origin Plugin</strong></a>, fourni sous licence  <a href="href=http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported</a>.';
+$txt['fancybox_title'] = 'FancyBox';
+$txt['fancybox_settings'] = 'Configuration du module FancyBox';
+$txt['fancybox_desc'] = 'Ce module ajoute un effet lumineux autour des images attachées ou insérées dans les posts.';
+
+// Effects for events
+$txt['fancybox_animation'] = 'Effet d\'animation';
+$txt['fancybox_effect_elastic'] = 'Effet élastique';
+$txt['fancybox_effect_fade'] = 'Fondu';
+$txt['fancybox_effect_none'] = 'Aucun';
+
+// Events we know, open/close/navigate
+$txt['fancybox_openEffect'] = 'Animation à l\'ouverture d\'une image';
+$txt['fancybox_closeEffect'] = 'Animation à la fermeture d\'une image';
+$txt['fancybox_navEffect'] = 'Animation lors du passage à l\'image suivante';
+$txt['fancybox_openSpeed'] = 'Vitesse de l\'animation à l\'ouverture';
+$txt['fancybox_closeSpeed'] = 'Vitesse de l\'animation à la fermeture';
+$txt['fancybox_navSpeed'] = 'Vitesse de l\'animation lors du passage à l\'image suivante';
+
+// Positioning options
+$txt['fancybox_panel_position'] = 'Position des boutons de navigation';
+$txt['fancybox_panel_top'] = 'Au-dessus';
+$txt['fancybox_panel_bottom'] = 'Au-dessous';
+$txt['fancybox_panel_simple'] = 'À l\'intérieur';
+$txt['fancybox_thumbnails'] = 'Afficher les vignettes sous les images';
+
+// Other
+$txt['fancybox_other'] = 'Autres options';
+$txt['fancybox_autoPlay'] = 'Démarrage automatique du diaporama';
+$txt['fancybox_playSpeed'] = 'Durée d\'affichage de chaque image';
+$txt['fancybox_bbc_img'] = 'Utiliser FancyBox avec les images insérées avec le tag [img]';
+$txt['fancybox_disable_img_in_url'] = 'Ne pas utiliser FancyBox avec les images insérées avec le tag [url]';
+$txt['fancybox_convert_photo_share'] = 'Convertir les liens vers des images pour afficher les photos en taille réelle';
+$txt['fancybox_convert_photo_share_help'] = 'Support limité pour imageshack, photobucket, radikal, fotosik, postimage, flic.kr, smugmug';
+$txt['fancybox_convert_postimage_share'] = 'Activer le support du site postimage';
+
+$txt['fancybox_displayOptions'] = 'Options d\'affichage';
+$txt['fancybox_Padding'] = 'Taille de la bordure autour de l\'image (0 pour aucune)';
+$txt['fancybox_Loop'] = 'Activer l\'affichage en boucle, donc réaffiche le premier élément à la suite du dernier';
+
+// Template strings for the gallery buttons
+$txt['fancy_text_error'] = 'Impossible d\'afficher l\'image.';
+$txt['fancy_button_next'] = 'Suivant';
+$txt['fancy_button_prev'] = 'Précédent';
+$txt['fancy_slideshow_start'] = 'Démarrer le diaporama';
+$txt['fancy_slideshow_pause'] = 'Mettre le diaporama en pause';
+$txt['fancy_toggle_size'] = 'Basculer la taille';
+?>
